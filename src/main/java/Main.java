29b47f4d4f5e469b1dc7a88875afcfb1d3b94f70
alicetheodora <alicetheodora@gmail.com>
2017-05-19 20:32:49 +0300

    complete product,transaction , modify dbconnect,main and create login,admin classes.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,36 @@ public class Main {
         }catch (Exception e){
             e.printStackTrace();
         }
+
         Shop main_shop = new Shop();
         System.out.println("End of program. Have a nice day!");
+
+        Client c= new Client();
+        System.out.println("Welcome"+c.getUsername());
+
+        System.out.println("Please select an option:");
+        System.out.println("1. View products by category / categories.\n" +
+                           "2. Order a product.\n" +
+                           "3. View shopping basket.\n" +
+                           "4. Confirm order. \n" +
+                           "5. Logout.");
+        int val=1;
+
+        switch(val) {
+            case 1 :
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+           default :
+                System.out.println("Invalid option");
+        }
+        System.out.println("Your option are: " + val);
     }
 }
+
